0b361be4b3e32586b2e2a88cdf977c6d9448668b
LiLPray <gandre@gaming.tech>
2024-09-14 15:56:04 +0200

    Maurad le boss

--- Assets/Scripts/Quest/QuestGiver.cs
+++ Assets/Scripts/Quest/QuestGiver.cs
@@ -1,12 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
 using System.Collections.Generic;
 
 public class QuestGiver : NetworkBehaviour
 {
-    public GameObject dialoguePanel; // Assign in the Inspector
-    public GameObject questSteps; // Assign in the Inspector
-    public GameObject rewardDialogue; // Assign in the Inspector
     public GameObject npc; // Assign in the Inspector
     public QuestManager questManager; // Assign in the Inspector
     public BoxCollider boxCollider; // Assign in the Inspector
@@ -21,24 +19,25 @@ public class QuestGiver : NetworkBehaviour
 
     private int questIndex; // L'index de la quête sera maintenant sélectionné aléatoirement
 
+    private Button acceptQuestButton; // Référence au bouton dans le Canvas du joueur
+
+    private void Start()
+    {
+
+    }
+
+
     private void Update()
     {
-        if (!isLocalPlayer) return;
-
-        if (playerInRange && Input.GetKeyDown(KeyCode.A))
+        if (!isLocalPlayer || acceptQuestButton == null) return;
+        if (questAccepted)
         {
-            if (questAccepted)
-            {
-                if (questManager.currentQuest != null && questManager.currentQuest.isComplete)
+            // Si la quête est terminée, activer le collider pour la récompense
+            if (questManager.currentQuest != null && questManager.currentQuest.isComplete)
                 {
                     CmdEnableCollider();
                 }
-            }
-            else
-            {
-                CmdAcceptQuest();
-            }
-        }
+        }     
     }
 
     [Command]
@@ -52,66 +51,68 @@ public class QuestGiver : NetworkBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("OnTriggerEnter");
-
-        if (!isLocalPlayer) return;
-
-        Debug.Log("OnTriggerEnter isLocalPlayer");
         if (other.CompareTag("Player") && !questAccepted)
         {
             Debug.Log("Player entered NPC trigger zone");
-            dialoguePanel.SetActive(true);
+            other.transform.Find("PlayerGui").Find("DialoguePanel").gameObject.SetActive(true);
             playerInRange = true;
+            GameObject acceptQuestButton = other.transform.Find("PlayerGui").Find("DialoguePanel").Find("AcceptButton").gameObject;
+
+            // Activer le bouton d'acceptation de quête
+            if (acceptQuestButton != null)
+            {
+                acceptQuestButton.gameObject.SetActive(true);
+                Debug.Log("AcceptButton activated");
+                acceptQuestButton.GetComponent<Button>().onClick.AddListener(CmdAcceptQuest); // Lier la méthode CmdAcceptQuest au clic du bouton
+                Debug.Log("AcceptButton activated2");
+            }
         }
 
         if (other.CompareTag("Player") && questAccepted && questManager.currentQuest != null && questManager.currentQuest.isComplete)
         {
-            rewardDialogue.SetActive(true);
+            other.transform.Find("PlayerGui").Find("RewardDialogue").gameObject.SetActive(true);
         }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    { 
-        Debug.Log("OncollisionEnter");
-
-
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (!isLocalPlayer) return;
-
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !questAccepted)
         {
             Debug.Log("Player exited NPC trigger zone");
-            dialoguePanel.SetActive(false);
+            other.transform.Find("PlayerGui").Find("DialoguePanel").gameObject.SetActive(false);
             playerInRange = false;
+            GameObject acceptQuestButton = other.transform.Find("PlayerGui").Find("DialoguePanel").Find("AcceptButton").gameObject;
+
+            // Désactiver le bouton d'acceptation de quête
+            if (acceptQuestButton != null)
+            {
+                acceptQuestButton.gameObject.SetActive(false);
+                acceptQuestButton.GetComponent<Button>().onClick.RemoveListener(CmdAcceptQuest); // Supprimer la méthode du bouton pour éviter les duplications
+            }
         }
     }
 
-    [Command]
+    [Command(requiresAuthority = false)]
     public void CmdAcceptQuest()
     {
-        Debug.Log("Quest accepted");
+        Debug.Log("CmdAcceptQuest called");
+        GameObject acceptQuestButton = connectionToClient.identity.transform.Find("PlayerGui").Find("DialoguePanel").Find("AcceptButton").gameObject;
 
-        if (dialoguePanel == null)
+        // Si le joueur ne possède pas la connexion sur cet objet
+        if (connectionToClient == null)
         {
-            Debug.LogError("dialoguePanel is not assigned!");
+            Debug.LogError("No connection to client!");
             return;
         }
 
-        if (questSteps == null)
-        {
-            Debug.LogError("questSteps is not assigned!");
-            return;
-        }
-
+        // Vérifie si le questManager est bien assigné
         if (questManager == null)
         {
             Debug.LogError("questManager is not assigned!");
             return;
         }
 
+        // Vérifie si des quêtes sont disponibles
         if (questManager.quests.Count == 0)
         {
             Debug.LogError("No quests available in questManager!");
@@ -121,18 +122,29 @@ public class QuestGiver : NetworkBehaviour
         // Sélectionner un questIndex aléatoire
         questIndex = Random.Range(0, questManager.quests.Count);
 
+        // Marquer la quête comme acceptée
         questAccepted = true;
+
+        // Démarrer la quête dans le QuestManager
         questManager.StartQuest(questIndex);
-        RpcUpdateQuestUI();
+
+        // Mettre à jour l'interface utilisateur de la quête
+        RpcUpdateQuestUI(connectionToClient);
+
+        // Désactiver le bouton après avoir accepté la quête
+        if (acceptQuestButton != null)
+        {
+            acceptQuestButton.gameObject.SetActive(false);
+        }
     }
+
+
+
 
     private void OnQuestAcceptedChanged(bool oldQuestAccepted, bool newQuestAccepted)
     {
         if (newQuestAccepted)
         {
-            dialoguePanel.SetActive(false);
-            questSteps.SetActive(true);
-
             if (boxCollider != null)
             {
                 boxCollider.enabled = false;
@@ -140,11 +152,13 @@ public class QuestGiver : NetworkBehaviour
         }
     }
 
-    [ClientRpc]
-    private void RpcUpdateQuestUI()
+    [TargetRpc]
+    private void RpcUpdateQuestUI(NetworkConnectionToClient player)
     {
+        Debug.Log("RpcUpdateQuestUI called");
         questManager.UpdateQuestUI();
     }
+
 
     [Command]
     public void CmdClaimReward()
@@ -171,17 +185,18 @@ public class QuestGiver : NetworkBehaviour
             }
         }
 
-        RpcHideQuestUI();
+        RpcHideQuestUI(connectionToClient);
 
         if (boxCollider != null) boxCollider.enabled = false;
         if (npc != null) npc.SetActive(false);
     }
 
-    [ClientRpc]
-    private void RpcHideQuestUI()
+    [TargetRpc]
+    private void RpcHideQuestUI(NetworkConnectionToClient player)
     {
-        dialoguePanel.SetActive(false);
-        questSteps.SetActive(false);
-        rewardDialogue.SetActive(false);
+        GameObject playerObject = player.identity.gameObject;
+        playerObject.transform.Find("PlayerGui").Find("DialoguePanel").gameObject.SetActive(false);
+        playerObject.transform.Find("PlayerGui").Find("QuestSteps").gameObject.SetActive(false);
+        playerObject.transform.Find("PlayerGui").Find("RewardDialogue").gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Quest/QuestManager.cs
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -33,35 +33,45 @@ public class Quest
 
 public class QuestManager : NetworkBehaviour
 {
-    public Text questDescriptionText; // Assign in the Inspector
-    public Text questStatusText; // Assign in the Inspector
-
     public List<Quest> quests = new List<Quest>();
     public Quest currentQuest; // La quête en cours
 
     [SyncVar(hook = nameof(OnCurrentQuestIndexChanged))]
     private int currentQuestIndex = -1; // L'indice de la quête en cours
 
+    private Text questDescriptionText;
+    private Text questStatusText;
+
     private void Start()
     {
-        if (!isLocalPlayer) return; // Only run on the local player instance
+        if (!isLocalPlayer) return; // Seulement pour l'instance locale du joueur
 
-        if (questDescriptionText == null || questStatusText == null)
+        // Assurez-vous que le QuestCanvas est désactivé au départ
+        GameObject playerCanva = GameObject.Find("QuestCanva");
+        if (playerCanva != null)
         {
-            Debug.LogError("Please assign the Text components in the Inspector.");
-            return;
+            playerCanva.SetActive(false);
+        }
+
+        // Trouver et assigner les composants Text du canvas QuestSteps
+        GameObject questStepsCanva = GameObject.Find("QuestSteps");
+        if (questStepsCanva != null)
+        {
+            questDescriptionText = questStepsCanva.transform.Find("QuestDescriptionText").GetComponent<Text>();
+            questStatusText = questStepsCanva.transform.Find("QuestStatusText").GetComponent<Text>();
         }
 
         if (quests.Count == 0)
         {
-            Debug.LogError("No quests available. Please assign quests in the Inspector.");
+            Debug.LogError("Aucune quête disponible. Merci d'assigner des quêtes dans l'inspecteur.");
         }
 
         if (quests.Count == 0)
         {
-            quests.Add(new Quest("Ouvir 1 coffres", "Ouvrir 1 coffres pour valider la quête.", 1));
-            quests.Add(new Quest("Ouvir 2 coffres", "Ouvrir 2 coffres pour valider la quête.", 2));
-            quests.Add(new Quest("Ouvir 3 coffres", "Ouvrir 3 coffres pour valider la quête.", 3));
+            // Ajouter des quêtes pour les tests si elles ne sont pas présentes
+            quests.Add(new Quest("Ouvrir 1 coffre", "Ouvrir 1 coffre pour valider la quête.", 1));
+            quests.Add(new Quest("Ouvrir 2 coffres", "Ouvrir 2 coffres pour valider la quête.", 2));
+            quests.Add(new Quest("Ouvrir 3 coffres", "Ouvrir 3 coffres pour valider la quête.", 3));
             quests.Add(new Quest("Vaincre 30 monstres", "Vaincre 30 monstres pour valider la quête.", 30));
             quests.Add(new Quest("Vaincre 20 monstres", "Vaincre 20 monstres pour valider la quête.", 20));
             quests.Add(new Quest("Vaincre 10 monstres", "Vaincre 10 monstres pour valider la quête.", 10));
@@ -73,43 +83,9 @@ public class QuestManager : NetworkBehaviour
 
     public void StartQuest(int questIndex)
     {
-        if (!isLocalPlayer) return; // Only the local player can request to start a quest
+        if (!isLocalPlayer) return; // Seul le joueur local peut démarrer une quête
 
         CmdStartQuest(questIndex);
-    }
-
-    [Command]
-    private void CmdStartQuest(int questIndex)
-    {
-        if (questIndex < 0 || questIndex >= quests.Count)
-        {
-            Debug.LogError("Invalid quest index! Quest index: " + questIndex);
-            return;
-        }
-
-        currentQuestIndex = questIndex;
-        quests[currentQuestIndex].isActive = true;
-
-        UpdateQuestUI();
-    }
-
-    public void CompleteQuest()
-    {
-        if (!isLocalPlayer) return; // Only the local player can request to complete a quest
-
-        CmdCompleteQuest();
-    }
-
-    [Command]
-    private void CmdCompleteQuest()
-    {
-        if (currentQuestIndex == -1) return;
-
-        Quest currentQuest = quests[currentQuestIndex];
-        currentQuest.isActive = false;
-        currentQuest.isComplete = true;
-
-        UpdateQuestUI();
     }
 
     public void UpdateQuestProgress(int amount)
@@ -120,6 +96,26 @@ public class QuestManager : NetworkBehaviour
     }
 
     [Command]
+    private void CmdStartQuest(int questIndex)
+    {
+        Debug.Log("CmdStartQuest appelé avec questIndex: " + questIndex);
+
+        if (questIndex < 0 || questIndex >= quests.Count)
+        {
+            Debug.LogError("Index de quête invalide! Quest index: " + questIndex);
+            return;
+        }
+
+        currentQuestIndex = questIndex;
+        quests[currentQuestIndex].isActive = true;
+
+        // Appelle la mise à jour de l'UI côté client
+        RpcUpdateQuestUI();
+
+        // Active le canvas de la quête côté client
+        RpcActivateQuestUI();
+    }
+
     private void CmdUpdateQuestProgress(int amount)
     {
         if (currentQuestIndex == -1) return;
@@ -140,6 +136,61 @@ public class QuestManager : NetworkBehaviour
     }
 
     [ClientRpc]
+    private void RpcActivateQuestUI()
+    {
+        GameObject playerCanva = GameObject.Find("QuestCanva");
+        if (playerCanva != null)
+        {
+            playerCanva.SetActive(true); // Active le canvas de la quête lorsqu'une quête est acceptée
+        }
+
+        GameObject RewardCanva = GameObject.Find("RewardDialogue");
+        if (RewardCanva != null)
+        {
+            RewardCanva.SetActive(false); // Désactive le dialogue de récompense au début
+        }
+
+        GameObject questStepsCanva = GameObject.Find("QuestSteps");
+        if (questStepsCanva != null)
+        {
+            questStepsCanva.SetActive(true); // Active le canvas des étapes de la quête
+        }
+    }
+
+    public void CompleteQuest()
+    {
+        if (!isLocalPlayer) return; // Seul le joueur local peut terminer une quête
+
+        CmdCompleteQuest();
+    }
+
+    [Command]
+    private void CmdCompleteQuest()
+    {
+        if (currentQuestIndex == -1) return;
+
+        Quest currentQuest = quests[currentQuestIndex];
+        currentQuest.isActive = false;
+        currentQuest.isComplete = true;
+
+        // Mise à jour de l'UI et activation du dialogue de récompense
+        RpcCompleteQuestUI();
+    }
+
+    [ClientRpc]
+    private void RpcCompleteQuestUI()
+    {
+        UpdateQuestUI(); // Mise à jour de l'UI
+
+        GameObject RewardCanva = GameObject.Find("RewardDialogue");
+        if (RewardCanva != null)
+        {
+            RewardCanva.SetActive(true); // Active le dialogue de récompense à la fin de la quête
+        }
+    }
+
+    // Cette méthode met à jour l'interface utilisateur de la quête en cours
+    [ClientRpc]
     private void RpcUpdateQuestUI()
     {
         UpdateQuestUI();
@@ -152,7 +203,7 @@ public class QuestManager : NetworkBehaviour
 
     public void UpdateQuestUI()
     {
-        if (currentQuestIndex != -1)
+        if (questDescriptionText != null && questStatusText != null && currentQuestIndex != -1)
         {
             Quest currentQuest = quests[currentQuestIndex];
             if (currentQuest.isActive)
